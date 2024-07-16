10b1bc29a2fee12294976b169d4ccac7dd876978
Siddesh Dhanlobhe <siddhesh112004@gmail.com>
2024-07-16 16:53:29 +0530

    adding des in note section

--- src/Componets/Homepage/Note/Note.jsx
+++ src/Componets/Homepage/Note/Note.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { useState } from 'react';
+import './customQuillStyles.css';
 
 function Note() {
+    const [desdata, setdesdata] = useState('siddesh is herer to ');
     return (
         <div className='w-full h-44 '>
             {/* You can open the modal using document.getElementById('ID').showModal() method */}
@@ -12,7 +17,7 @@ function Note() {
                         <h2 className="  w-full h-full  flex justify-center items-center overflow-x-auto p-3 font-semibold">
                             title
                         </h2>
-                        
+
                     </div>
                 </div>
 
@@ -20,9 +25,18 @@ function Note() {
             <dialog id="my_modal_4" className="modal">
                 <div className="modal-box  max-w-full">
                     <h3 className="font-bold text-lg">Hello!</h3>
-                    <p className="py-4">
-                        Your amazing application description goes here.
-                    </p>
+                    <div className='mt-3'>
+                        <div className='font-bold'>Description</div>
+                        <ReactQuill
+                            className=' text-white custom-quill'
+                            theme="snow"
+                            value={desdata}
+                            readOnly={true}
+                            modules={{ toolbar: false }}
+
+                            
+                        />
+                    </div>
                     <div className="modal-action">
                         <form method="dialog">
                             {/* if there is a button, it will close the modal */}
